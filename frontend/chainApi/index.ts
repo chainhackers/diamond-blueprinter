bb4c91e4716d637a585f5cabbf0cc60b882d5907
elizavetta <elizavetta.net@gmail.com>
2023-03-26 18:46:11 +0300

    test mantle deploy

--- frontend/chainApi/index.ts
+++ frontend/chainApi/index.ts
@@ -3,11 +3,11 @@ import { fetchSigner } from '@wagmi/core';
 
 export async function getAbi(address: string) {
   try {
-    const uri = `https://api.polygonscan.com/api?module=contract&action=getabi&address=${address}&format=raw&apikey=${process.env.NEXT_PUBLIC_POLYGONSCAN_API_KEY}`;
-
+    //const uri = `https://api.polygonscan.com/api?module=contract&action=getabi&address=${address}&apikey=${process.env.NEXT_PUBLIC_POLYGONSCAN_API_KEY}`;
+    const uri = `https://explorer.testnet.mantle.xyz/api?module=contract&action=getabi&address=${address}&apikey=${process.env.NEXT_PUBLIC_POLYGONSCAN_API_KEY}`
     console.log(uri);
     const response = await fetch(uri);
-    return response.json();
+    return response.json().result;
   } catch (error) {
     console.log('getAbi error', error);
   }
