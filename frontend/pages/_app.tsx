117971fa922ef7ec9810afac3ed124d42a516955
elizavetta <elizavetta.net@gmail.com>
2023-03-26 18:24:06 +0300

    mantle chain connected to wagmi

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -4,16 +4,52 @@ import type { AppProps } from 'next/app';
 import '@rainbow-me/rainbowkit/styles.css';
 import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { configureChains, createClient, WagmiConfig } from 'wagmi';
-import { polygon } from 'wagmi/chains';
+import { Chain, polygon } from 'wagmi/chains';
+//import { polygon } from '@wagmi/core/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 
 import { Layout } from '@/components';
 import { DiamondContextProvider } from '@/contexts';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
 
-const { chains, provider, webSocketProvider } = configureChains(
-  [polygon],
-  [alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY! })],
+
+const mantleChain: Chain = {
+  id: 5001,
+  name: 'Mantle',
+  network: 'mantle',
+  iconUrl: 'https://example.com/icon.svg',
+  iconBackground: '#fff',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Mantle',
+    symbol: 'MNTL',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc.testnet.mantle.xyz/'],
+    },
+    public: {http: ['http://rpc.testnet.mantle.xyz/']}
+  },
+  blockExplorers: {
+    default: { name: 'SnowTrace', url: 'https://snowtrace.io' },
+    etherscan: { name: 'SnowTrace', url: 'https://snowtrace.io' },
+  },
+  testnet: true,
+};    
+
+// const { chains, provider, webSocketProvider } = configureChains(
+//   [polygon, mantleChain],
+//   [alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY! })],
+// );
+
+const { provider, chains, webSocketProvider } = configureChains(
+  [mantleChain],
+  [
+    jsonRpcProvider({
+      rpc: chain => ({ http: 'https://rpc.testnet.mantle.xyz/' }),
+    }),
+  ]
 );
 
 const { connectors } = getDefaultWallets({
